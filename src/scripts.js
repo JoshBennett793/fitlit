ee1d69306a3409e1935a390c4611b55d2bd133cc
Jen Nguyen <jenniferannnguyen@live.com>
2023-07-09 20:53:17 -0600

    Rebase main onto css-edits

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,7 @@
+import { showUserData } from './domUpdates'
+showUserData('hey')
+
+// An example of how you tell webpack to use a CSS file
 import './css/styles.css';
 import './images/turing-logo.png';
 
