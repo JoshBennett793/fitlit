b3630a3f8ad278debe19fd76e36c31d206808f2d
Josh Bennett <joshbennett793@yahoo.com>
2023-07-24 22:56:39 -0500

    Combine getActivityDataByDate & getToday => getDataByDate
    In this commit, getUserData was shortened by reversing the guard clause.
    Additionally, getActivityDataByDate was combined with getToday because
    they were effectively doing the same thing. The resulting function was
    renamed getDataByDate.

--- src/model.js
+++ src/model.js
@@ -1,23 +1,21 @@
 /* User Data */
 
-export function getUserData(dataType, users, id) {
-  if (
-    dataType === 'hydrationData' ||
-    dataType === 'sleepData' ||
-    dataType === 'activityData'
-  ) {
-    return users.filter(data => data.userID === id);
+export function getUserData(dataType, dataset, id) {
+  if (dataType === 'users') {
+    return dataset.find(data => data.id === id);
   } else {
-    return users.find(data => data.id === id);
+    return dataset.filter(data => data.userID === id);
   }
-}
-
-function getRandomID(array) {
-  return Math.floor(Math.random() * array.length) + 1;
 }
 
 export function getRandomUser(users) {
   return getUserData('users', users, getRandomID(users));
+}
+
+/* Utility Functions */
+
+function getRandomID(array) {
+  return Math.floor(Math.random() * array.length) + 1;
 }
 
 export function getCurrentDate(userData) {
@@ -39,11 +37,15 @@ export function getAllTimeAverage(key, userData) {
   }
 }
 
-export function getTodays(key, userData, date) {
+export function getDataByDate(key, userData, date) {
   userData = userData.find(data => data.date === date);
 
   if (!userData) {
     return 0;
+  }
+
+  if (key === 'activityData') {
+    return userData;
   }
 
   return userData[key];
@@ -76,12 +78,6 @@ export function getUserStepGoal(user) {
 
 /* Activity Data */
 
-export function getActivityDataByDate(activityData, id, date) {
-  return getUserData('activityData', activityData, id).find(
-    data => data.date === date
-  );
-}
-
 export function getMinutesActive(activityData) {
   return activityData.minutesActive;
 }
@@ -96,9 +92,18 @@ export function compareStepsWithGoal(stepData, goal) {
 
 // Accepts a single user's data as userData param
 // Accepts all activity data
-export function calculateDistanceTraveled(userData, date = '2023/07/01', activityData) {
+export function calculateDistanceTraveled(
+  userData,
+  date = '2023/07/01',
+  activityData,
+) {
+  // 1 mile === 5280 feet
   const mile = 5280;
   activityData = getActivityDataByDate(activityData, userData.id, date);
   const distance = (userData.strideLength * activityData.numSteps) / mile;
   return parseFloat(distance.toFixed(2));
+}
+
+export function getListOfFriends(user) {
+  return;
 }
