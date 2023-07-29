e8423525d3e4df55ac445e5559922adfc4479f13
Josh Bennett <joshbennett793@yahoo.com>
2023-07-29 14:21:16 -0500

    Move sleep form handler fn to model file

--- src/model.js
+++ src/model.js
@@ -1,3 +1,6 @@
+import { store, getAllApiData } from './scripts';
+import { setApiData } from './apiCalls';
+
 // Utility Functions
 
 function getRandomID(array) {
@@ -131,4 +134,50 @@ export function calculateTotalTimeSlept(...times) {
   }
 
   return (diffHour + diffMin / 60).toFixed(1);
+}
+
+export function storeSleepData() {
+  const sleepModalForm = document.querySelector('form');
+
+  const formData = new FormData(sleepModalForm);
+  const values = [...formData.entries()];
+  const formattedValues = values.reduce((acc, value) => {
+    acc[value[0]] = value[1];
+    return acc;
+  }, {});
+
+  const totalTimeSlept = calculateTotalTimeSlept(
+    formattedValues['begin-time'],
+    formattedValues['end-time'],
+  );
+
+  const user = store.getKey('user');
+  const sleepData = getUserData(
+    'sleepData',
+    store.getKey('sleepData'),
+    user.id,
+  );
+  const nextDay = new Date(getCurrentDate(sleepData));
+  nextDay.setDate(nextDay.getDate() + 1);
+  const year = nextDay.toLocaleString('default', { year: 'numeric' });
+  const month = nextDay.toLocaleString('default', { month: '2-digit' });
+  const day = nextDay.toLocaleString('default', { day: '2-digit' });
+  const date = `${year}/${month}/${day}`;
+
+  setApiData(
+    store.getAPIKey('sleep'),
+    user.id,
+    date,
+    parseFloat(totalTimeSlept),
+    Number(formattedValues['sleep-quality']),
+  )
+    .then(() => {
+      store.getKey('todaysStepDataChart').destroy();
+      store.getKey('weeklyStepDataChart').destroy();
+      store.getKey('weeklySleepDataChart').destroy();
+      store.getKey('weeklySleepQualityChart').destroy();
+      store.getKey('weeklyHydrationDataChart').destroy();
+      getAllApiData(store.getKey('user'));
+    })
+    .catch(err => console.log(err));
 }
