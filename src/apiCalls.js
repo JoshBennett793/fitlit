cf063b3a1c234a19eceeb8725f2572794fb0bf79
Josh Bennett <joshbennett793@yahoo.com>
2023-07-29 14:37:27 -0500

    Handle response statuses when other than ok

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,5 @@
+import { displayError } from './domUpdates';
+
 export function getApiData(url, dataKey) {
   return fetch(url)
     .then(response => response.json())
@@ -17,7 +19,29 @@ export function setApiData(url, userID, date, hoursSlept, sleepQuality) {
       'Content-Type': 'application/json',
     },
   })
-    .then(response => response.json())
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      // IF FORM IS SUBMITTED WITH MISSING INFORMATION
+      if (response.status === 422) {
+        throw new Error('The form is missing 1 or more pieces of information.');
+        // NETWORK ERROR
+      } else if (response.status >= 500) {
+        throw new Error(
+          `There has been a network error: ${response.status} ${response.statusText}. Please refresh the page or try again later.`,
+        );
+      } else {
+        // ALL OTHER ERRORS
+        throw new Error(
+          `There that been an error: ${response.status} ${response.statusText}`,
+        );
+      }
+    })
     .then(data => data)
-    .catch(error => console.log('error', error));
+    .catch(error => {
+      console.log(error);
+      displayError(error);
+    });
 }
